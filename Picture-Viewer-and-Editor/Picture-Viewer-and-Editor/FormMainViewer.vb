4c1820c136c0985208cb900d0cb956df8159d3a5
Logan Tellier <logan.tellier@outlook.com>
2019-09-30 21:55:22 -0400

    Functionality for opening a picture file with the open button.
    Added function to handle the "open" click event. Initiated a region for helper functions, one of which will be that to display an image once selected.

--- Picture-Viewer-and-Editor/Picture-Viewer-and-Editor/FormMainViewer.vb
+++ Picture-Viewer-and-Editor/Picture-Viewer-and-Editor/FormMainViewer.vb
@@ -1,5 +1,35 @@
-﻿Public Class FormMainViewer
+﻿Imports System.IO
+
+Public Class FormMainViewer
+
+#Region "ButtonClicks"
+
+    Private Sub Button_Open_Click(sender As Object, e As EventArgs) Handles Button_Open.Click
+
+        Dim pictureFileDialog As New OpenFileDialog
+
+        Dim debugPath As String = Directory.GetCurrentDirectory()
+        Dim projectPath As String = Directory.GetParent(debugPath).Parent.FullName
+        Dim totalPath As String = System.IO.Path.Combine(projectPath, "..\\TestingResources")
+
+        With pictureFileDialog
+            .InitialDirectory = System.IO.Path.GetFullPath(totalPath)
+            .RestoreDirectory = True
+            .Title = "Select an Image File"
+        End With
+
+        If pictureFileDialog.ShowDialog() = DialogResult.OK Then
+
+        End If
+
+    End Sub
+
+#End Region
+
+#Region "Helper Functions"
 
 
+
+#End Region
 
 End Class
